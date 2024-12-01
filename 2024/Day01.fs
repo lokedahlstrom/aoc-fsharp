652d741cd83abf429f8b10defe36ee5d4dafde80
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-01 10:54:44 +0100

    raw solution for day 1

--- 2024/Day01.fs
+++ 2024/Day01.fs
@@ -1,11 +1,47 @@
 module Year2024.Day01
 
+open System
+open System.Collections.Generic
+open System.Text.RegularExpressions
+
 let input = Client.getInput 2024 1
 
-let solve = 
-    input |> ignore
-    0
+let left = ResizeArray([])
+let right = ResizeArray([])
+let appearances = Dictionary<int, int>()
+let regex = Regex(@"\d+", RegexOptions.Compiled)
+    
+let collect (s: string) =
+    let matches = regex.Matches(s)
+    let lVal = int matches[0].Value
+    let rVal = int matches[1].Value
+    left.Add(lVal)
+    right.Add(rVal)
+    
+    match appearances.TryGetValue(rVal) with
+    | true, value -> appearances.[rVal] <- value + 1
+    | false, _ -> appearances.[rVal] <- 1
+    
+let totalDistance =
+    input
+    |> Seq.iter collect
+    
+    left.Sort()
+    right.Sort()
+    
+    left
+    |> Seq.mapi (fun index value -> abs (value - right[index]))
+    |> Seq.sum
+
+let similarityScore = 
+    left
+    |> Seq.sumBy (fun value ->
+        value *
+            match appearances.TryGetValue(value) with
+            | true, v -> v
+            | false, _ -> 0
+        )
 
 let run () =
-    printfn $"Day 01, Part one: %d{solve}"
-    printfn $"Day 01, Part two: %d{solve}"
+    printfn $"Day 01, Part one: %d{totalDistance}"
+    printfn $"Day 01, Part two: %d{similarityScore}"
