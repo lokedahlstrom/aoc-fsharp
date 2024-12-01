d97bb57b6ae633a17a9b7bf2586cb9beb53c6b62
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-01 13:21:16 +0100

    added stub for day 2

---
+++ 2024/Day02.Tests.fs
@@ -0,0 +1,6 @@
+module Tests.Year2024.Day02
+open Xunit
+
+[<Fact>]
+let ``Sample-Rows-PartOne`` () =
+    Assert.True(true)
---
+++ 2024/Day02.fs
@@ -0,0 +1,7 @@
+module Year2024.Day02
+
+let input = Client.getInput 2024 2
+
+let run () =
+    printfn $"Day 01, Part one: %d{0}"
+    printfn $"Day 01, Part two: %d{0}"
