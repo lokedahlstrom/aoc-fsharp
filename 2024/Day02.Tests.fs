56430dd1d11cfecaedb96b59fd45cbda8c584826
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-02 17:30:32 +0100

    day two

--- 2024/Day02.Tests.fs
+++ 2024/Day02.Tests.fs
@@ -1,6 +1,50 @@
 module Tests.Year2024.Day02
 open Xunit
+open Year2024.Day02
 
+let sample = """
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+"""
+
+let readSample () =
+    sample.Trim().Split("\n")
+    
+[<Theory>]
+[<InlineData("7 6 4 2 1", true)>]
+[<InlineData("1 2 7 8 9", false)>]
+[<InlineData("9 7 6 2 1", false)>]
+[<InlineData("1 3 2 4 5", false)>]
+[<InlineData("8 6 4 4 1", false)>]
+[<InlineData("1 3 6 7 9", true)>]
+let ``Safety Part One`` (line: string, expected) =
+    let ints = stringToInts line
+    Assert.Equal(expected, reportIsSafe ints)
+    
+[<Theory>]
+[<InlineData("7 6 4 2 1", true)>]
+[<InlineData("1 2 7 8 9", false)>]
+[<InlineData("9 7 6 2 1", false)>]
+[<InlineData("1 3 2 4 5", true)>]
+[<InlineData("8 6 4 4 1", true)>]
+[<InlineData("1 3 6 7 9", true)>]
+[<InlineData("5 0 1 2 3", true)>]
+[<InlineData("1 9 8 7 6", true)>]
+[<InlineData("1 1 8 7 6", false)>]
+[<InlineData("61 63 64 67 69 72 74 81", true)>]
+[<InlineData("84 85 88 89 90 85", true)>]
+let ``Safety Part Two`` (line: string, expected) =
+    let ints = stringToInts line
+    Assert.Equal(expected, reportIsSafe2 ints)
+    
 [<Fact>]
 let ``Sample-Rows-PartOne`` () =
-    Assert.True(true)
+    Assert.Equal(2, partOne(readSample()))
+
+[<Fact>]
+let ``Sample-Rows-PartTwo`` () =
+    Assert.Equal(4, partTwo(readSample()))
--- 2024/Day02.fs
+++ 2024/Day02.fs
@@ -1,7 +1,59 @@
 module Year2024.Day02
 
-let input = Client.getInput 2024 2
+open System
+open System.Security.AccessControl
 
+let input () = Client.getInput 2024 2
+
+let stringToInt s = int s
+    
+let stringToInts (line: string) =
+    line.Split(" ")
+    |> Array.map stringToInt
+    
+let safeDifference (l: int, r: int) =
+    let delta = abs (l - r)
+    delta = 1 || delta <= 3
+    
+let reportIsSafe (report: int[]) =
+    let pairs = report |> Seq.pairwise
+    
+    if pairs |> Seq.forall (fun (prev, cur) -> (cur > prev) && (safeDifference (cur, prev))) then
+        true
+    else
+        pairs |> Seq.forall (fun (prev, cur) -> (cur < prev) && (safeDifference (cur, prev)))
+
+let partOne data =
+    data
+    |> Seq.map stringToInts
+    |> Seq.fold (fun acc cur ->
+        if reportIsSafe cur then
+            acc + 1
+        else
+            acc
+    ) 0
+    
+let reportIsSafe2 (report: int[]) =
+    if not (reportIsSafe report) then
+        report
+        |> Seq.mapi (fun index _ -> index)
+        |> Seq.exists (fun i ->
+            let newReport = Array.append (report[..i-1]) (report[i+1..])
+            reportIsSafe newReport
+            )
+    else
+        true
+        
+let partTwo data =
+    data
+    |> Seq.map stringToInts
+    |> Seq.fold (fun acc cur ->
+        if reportIsSafe2 cur then
+            acc + 1
+        else
+            acc
+    ) 0
+    
 let run () =
-    printfn $"Day 01, Part one: %d{0}"
-    printfn $"Day 01, Part two: %d{0}"
+    printfn $"Day 01, Part one: %d{partOne (input())}"
+    printfn $"Day 01, Part two: %d{partTwo (input())}"
