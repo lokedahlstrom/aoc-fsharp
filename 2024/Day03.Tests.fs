fe86578f8367ec9c6ad49fe66dfefa76b0a2006b
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-03 07:40:51 +0100

    day three completed

--- 2024/Day03.Tests.fs
+++ 2024/Day03.Tests.fs
@@ -1,24 +1,25 @@
 module Tests.Year2024.Day03
+open System.Text.RegularExpressions
 open Xunit
 open Year2024.Day03
 
 let sample = """
+xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))
+"""
 
+let sample2 = """
+xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
 """
 
 let readSample () =
     sample.Trim().Split("\n")
     
-[<Theory>]
-[<InlineData("", true)>]
-let ``Part One`` (line: string, expected) =
-    Assert.Equal(expected, true)
-    
-
 [<Fact>]
 let ``Sample-Rows-PartOne`` () =
-    Assert.Equal(0, partOne(readSample()))
+    let result = partOne (readSample())
+    Assert.Equal(161, result)
 
 [<Fact>]
 let ``Sample-Rows-PartTwo`` () =
-    Assert.Equal(0, partTwo(readSample()))
+    Assert.Equal(48, partTwo(sample2.Split("\n")))
+    
--- 2024/Day03.fs
+++ 2024/Day03.fs
@@ -1,17 +1,58 @@
 module Year2024.Day03
+open System.Text.RegularExpressions
 
-let input () = Client.getInput 2024 2
+let input () = Client.getInput 2024 3
+
+let mulRegex = Regex(@"mul\(\d+\,\d+\)", RegexOptions.Compiled)
+let digitsRegex = Regex(@"\d+", RegexOptions.Compiled)
+let opRegex = Regex(@"don\'t\(\)|do\(\)|mul\(\d+\,\d+\)", RegexOptions.Compiled)
+
+let multiply (s: string) =
+    digitsRegex.Matches(s)
+    |> Seq.map (fun (m: Match) -> Helpers.stringToInt(m.Value))    
+    |> Seq.pairwise
+    |> Seq.fold (fun acc (x, y) -> (x * y)) 0
+    
+let findOperations s =
+    mulRegex.Matches(s)
+    |> Seq.map (_.Value)
+
+let findOperationsPartTwo s =
+    opRegex.Matches(s)
+    |> Seq.map (_.Value)
+    
+let processOperation (s: string) =
+    findOperations s
+    |> Seq.map multiply
+    |> Seq.fold (fun acc cur -> acc + cur) 0
 
 let partOne data =
     data
-    |> ignore
-    0
+    |> Seq.fold (fun acc line -> acc + processOperation line) 0 
 
 let partTwo data =
+    let mutable isOn = true
+    let mutable result = 0
+    
     data
-    |> ignore
-    0
-
+    |> Seq.map findOperationsPartTwo
+    |> Seq.iter (fun operations ->
+        operations
+        |> Seq.iter (fun op ->            
+            if op.Contains("don't") then
+                isOn <- false
+            else if op.Contains("do") then
+                isOn <- true
+            else if isOn then
+                let dms = digitsRegex.Matches(op)
+                let left = int dms[0].Value
+                let right = int dms[1].Value
+                
+                result <- result + (left * right)
+            )
+        )
+    result
+    
 let run () =
     printfn $"Day 03, Part one: %d{partOne (input())}"
     printfn $"Day 03, Part two: %d{partTwo (input())}"
