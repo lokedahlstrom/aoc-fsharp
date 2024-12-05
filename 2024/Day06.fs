ee4f3979c9b9949273ca0e1c0807f5e73bc4e847
Loke Dahlström <loke.dahlstroem@eon.com>
2024-12-05 22:05:51 +0100

    added stub for day 6

---
+++ 2024/Day06.fs
@@ -0,0 +1,17 @@
+module Year2024.Day06
+
+let input () = Client.getInput 2024 6
+        
+let partOne data =  
+    data
+    |> ignore
+    0
+        
+let partTwo data =
+    data
+    |> ignore
+    0
+    
+let run () =
+    printfn $"Day 06, Part one: %d{partOne (input())}"
+    printfn $"Day 06, Part two: %d{partTwo (input())}"
