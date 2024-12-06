f78acf9004ca0a577e403b5db13a23fcc73f595c
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-06 22:27:47 +0100

    day 6

--- 2024/Day06.Tests.fs
+++ 2024/Day06.Tests.fs
@@ -4,15 +4,54 @@ open Year2024.Day06
 open Helpers
 
 let sample = """
+....#.....
+.........#
+..........
+..#.......
+.......#..
+..........
+.#..^.....
+........#.
+#.........
+......#...
 """
 
 let readSample () =
     sample.Trim().Split("\n")
     
 [<Fact>]
+let ``Is Obstacle`` () =
+    let map = parseGrid(readSample())
+    Assert.True(map[0][4] = Obstacle)
+    
+[<Fact>]
+let ``Find Guard`` () =
+    let map = parseGrid(readSample())
+    match findStartPos (map, Guard) with
+    | Some(y, x) -> Assert.Equal((6, 4), (y, x))
+    | None -> Assert.Fail()
+    
+[<Fact>]
 let ``Sample-Rows-PartOne`` () =
-    Assert.Equal(0, partOne(readSample()))
+    let _, count = partOne(readSample())
+    Assert.Equal(41, count)
+
+[<Fact>]
+let ``Rotation of Delta`` () =
+    let north = (-1, 0)
+    
+    let east = rotate90CW north
+    let south = rotate90CW east
+    let west = rotate90CW south
+    let north = rotate90CW west
+    
+    Assert.Equal((0, 1), east)
+    Assert.Equal((1, 0), south)
+    Assert.Equal((0, -1), west)
+    Assert.Equal((-1, 0), north)
+
 
 [<Fact>]
 let ``Sample-Rows-PartTwo`` () =
-    Assert.Equal(0, partTwo(readSample()))
+    let visited, _ = partOne(readSample())
+    Assert.Equal(6, partTwo(readSample(), visited))
--- 2024/Day06.fs
+++ 2024/Day06.fs
@@ -1,17 +1,116 @@
 module Year2024.Day06
 
+open System.Collections.Generic
+open Helpers
+
 let input () = Client.getInput 2024 6
-        
-let partOne data =  
+
+let Guard = '^'
+let Obstacle = '#'
+
+let ternary (predicate: bool, whenTrue: Option<'T>, whenFalse: Option<'T>) =
+    if predicate then whenTrue else whenFalse
+
+let ifthenelse (predicate: bool, whenTrue, whenFalse) =
+    if predicate then whenTrue else whenFalse
+
+let parseGrid (data: string[]) =
     data
-    |> ignore
-    0
+    |> Seq.map (fun line -> line.ToCharArray())
+    |> Seq.toArray
+    
+let findStartPos (map: char[][], target: char) =
+    map
+    |> Array.mapi (fun y row ->
+        row
+        |> Array.mapi (fun x c -> ternary(c = target, Some(y, x), None))
+        |> Array.choose id
+    )
+    |> Array.collect id 
+    |> Array.tryHead
+
+let rotate90CW (y, x) = (x, -y)
+
+let createKey (y, x, (a, b)) =
+    $"{y},{x},{a},{b}"
+
+let partOne data =
+    let map = parseGrid data
+    let visited = Dictionary()
+      
+    let move (y: int, x:int, d) =
+        let mutable newD = d
+        let mutable ny = y
+        let mutable nx = x
         
-let partTwo data =
-    data
-    |> ignore
-    0
+        while ny >= 0 && nx >= 0 && ny < map.Length && nx < map[0].Length do
+            let key = createKey(ny, nx, d)
+            
+            visited.TryAdd (key, true) |> ignore 
+            
+            let testY = ny+fst newD
+            let testX = nx+snd newD
+            if testY >= 0 && testX >= 0 && testY < map.Length && testX < map[0].Length && map[ny+fst newD][nx+snd newD] = Obstacle then
+                newD <- rotate90CW(newD)
+            
+            ny <- ny + fst newD
+            nx <- nx + snd newD
+        visited, visited.Count
+
+    match findStartPos (map, Guard) with
+    | Some(y, x) ->
+        let res = move (y, x, (-1, 0))
+        res
+    | None -> Dictionary(), 0
+        
+let partTwo (data, visited: Dictionary<string, bool>) =
+    let map = parseGrid data
+    
+    let W = map[0].Length
+    let H = map.Length
+    
+    let move (y: int, x:int, d) =
+        let mutable newD = d
+        let mutable ny = y
+        let mutable nx = x
+        let visited = Dictionary()
+        let mutable cycle = false
+        
+        while not cycle && ny >= 0 && nx >= 0 && ny < map.Length && nx < map[0].Length do
+            let key = createKey(ny,nx,newD)
+            if visited.ContainsKey key then
+                visited[key] <- visited[key]+1
+            visited.TryAdd (key, 1) |> ignore 
+            
+            let testY = ny+fst newD
+            let testX = nx+snd newD
+            
+            if testY >= 0 && testX >= 0 && testY < map.Length && testX < map[0].Length && map[ny+fst newD][nx+snd newD] = Obstacle then
+                newD <- rotate90CW(newD)
+
+            ny <- ny + fst newD
+            nx <- nx + snd newD
+                
+        cycle
+
+    match findStartPos (map, Guard) with
+    | Some(y, x) ->
+        let key = createKey(y,x,(-1,0))
+        visited.Remove(key) |> ignore
+        let mutable cycles = 0
+        visited.Keys
+        |> Seq.iter (fun k ->
+            let pos = k.Split(",") |> Seq.take(2) |> Seq.map (fun s -> int s) |> Seq.toArray
+            if map[pos[0]][pos[1]] = '.' then 
+                map[pos[0]][pos[1]] <- Obstacle
+                let res = move (y, x, (-1, 0))
+                if res then cycles <- cycles + 1
+                map[pos[0]][pos[1]] <- '.'
+            )
+        cycles
+    | None -> 0
     
 let run () =
-    printfn $"Day 06, Part one: %d{partOne (input())}"
-    printfn $"Day 06, Part two: %d{partTwo (input())}"
+    let visited, count = partOne (input())
+    printfn $"Day 06, Part one: %d{count}"
+    printfn $"Day 06, Part two: %d{partTwo (input(), visited)}"
