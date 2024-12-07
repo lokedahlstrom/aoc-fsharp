dc97ce87af82848ca30d3b9a176393f11c01bce4
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-07 19:21:43 +0100

    day 7

--- 2024/Day07.Tests.fs
+++ 2024/Day07.Tests.fs
@@ -3,15 +3,24 @@ open Xunit
 open Year2024.Day07
 
 let sample = """
+190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
 """
 
 let readSample () =
     sample.Trim().Split("\n")
-
+    
 [<Fact>]
 let ``Sample-Rows-PartOne`` () =
-    Assert.Equal(0, partOne(readSample()))
+    Assert.Equal(3749I, partOne(readSample()))
 
 [<Fact>]
 let ``Sample-Rows-PartTwo`` () =
-    Assert.Equal(0, partTwo(readSample()))
+    Assert.Equal(11387I, partTwo(readSample()))
--- 2024/Day07.fs
+++ 2024/Day07.fs
@@ -1,18 +1,42 @@
 module Year2024.Day07
-open Helpers
 
 let input () = Client.getInput 2024 7
+
+let parseLine (line: string) =
+    line.Split([| ':'; ' ' |])
+    |> Array.filter (fun t -> t.Length > 0)
+    |> Array.map bigint.Parse
+
+let mul (l, r) = l * r
+let add (l, r) = l + r
+let cct (l, r) = bigint.Parse(string l + string r)
+
+let parseData (data: string[]) = data |> Array.map parseLine
+
+let rec solvable (ops, expected: bigint, numbers: bigint[], total: bigint, i) =
+    if i >= numbers.Length then
+        expected = total
+    else
+        ops |> Array.exists (fun op -> solvable (ops, expected, numbers, op(total, numbers[i]), i+1))
+
+let solve operations data =
+    parseData data
+    |> Array.filter (fun line ->
+        let expected = line[0]
+        let numbers = line[1..]
+        let total = numbers[0]
         
-let partOne data =  
-    data
-    |> ignore
-    0
-        
+        solvable(operations, expected, numbers, total, 1))
+    |> Array.sumBy (fun eq -> eq[0])
+    
+let partOne data =
+    let ops = [| mul; add |]
+    solve ops data
+
 let partTwo data =
-    data
-    |> ignore
-    0
+    let ops = [| mul; add; cct |]
+    solve ops data
     
 let run () =
-    printfn $"Day 07, Part one: %d{partOne (input())}"
-    printfn $"Day 07, Part two: %d{partTwo (input())}"
+    printfn $"Day 07, Part one: %A{partOne (input())}"
+    printfn $"Day 07, Part two: %A{partTwo (input())}"
