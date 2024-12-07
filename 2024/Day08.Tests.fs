3741bf8ac406a629ba5ca72b0c4303a241e680ce
Loke Dahlström <loke.dahlstrom@gmail.com>
2024-12-07 19:24:26 +0100

    added stub for day 8

---
+++ 2024/Day08.Tests.fs
@@ -0,0 +1,18 @@
+module Tests.Year2024.Day08
+open Xunit
+open Year2024.Day08
+open Helpers
+
+let sample = """
+"""
+
+let readSample () =
+    sample.Trim().Split("\n")
+    
+[<Fact>]
+let ``Sample-Rows-PartOne`` () =
+    Assert.Equal(0, partOne(readSample()))
+
+[<Fact>]
+let ``Sample-Rows-PartTwo`` () =
+    Assert.Equal(0, partTwo(readSample()))
---
+++ 2024/Day08.fs
@@ -0,0 +1,17 @@
+module Year2024.Day08
+
+let input () = Client.getInput 2024 8
+        
+let partOne data =  
+    data
+    |> ignore
+    0
+        
+let partTwo data =
+    data
+    |> ignore
+    0
+    
+let run () =
+    printfn $"Day 08, Part one: %d{partOne (input())}"
+    printfn $"Day 08, Part two: %d{partTwo (input())}"
